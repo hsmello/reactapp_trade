48771c9068b17f51198e3a2c318fb8c5c9ac0afb
ff <fanuelson@gmail.com>
2020-02-08 18:54:56 -0300

    filters to data file added to graph one

--- src/pages/Analysis.jsx
+++ src/pages/Analysis.jsx
@@ -5,13 +5,15 @@ import HighchartsReact from "highcharts-react-official";
 import HC_exporting from 'highcharts/modules/exporting';
 import Papa from 'papaparse';
 import arquivo from '../data-files/TradeDataAnalysis.csv';
+import { Dropdown } from 'primereact/dropdown';
+import { Button } from 'primereact/button';
 
 highcharts3d(Highcharts);
 HC_exporting(Highcharts);
 
 let options = {
     chart: {
-      animation: false,
+      animation: true,
       type: 'pie',
       options3d: {
         enabled: true,
@@ -33,24 +35,32 @@ let options = {
     series: [{
       name: 'Delivered amount',
       data: [
-        ['Bananas', 8],
-        ['Kiwi', 3],
-        ['Mixed nuts', 1],
-        ['Oranges', 6],
-        ['Apples', 8],
-        ['Pears', 4],
-        ['Clementines', 4],
-        ['Reddish (bag)', 1],
-        ['Grapes (bunch)', 1]
+        
       ]
     }]
   }
+
+const yearOptions = [
+  {label: '2014', value: '2014'},
+  {label: '2015', value: '2015'},
+  {label: '2016', value: '2016'},
+  {label: '2017', value: '2017'}
+]
+
+const tradeFlowOptions = [
+  {label: 'Export', value: 'Export'},
+  {label: 'Import', value: 'Import'}
+]
 
 class Analysis extends Component {
   constructor(props){
     super(props);
     this.state = {
-      graphicOptions: options
+      graphicOptions: options,
+      yearSelected: null,
+      tradeFlowSelected: null,
+      fileRows: [],
+      showGraph: true
     }
   }
   
@@ -68,10 +78,14 @@ class Analysis extends Component {
   PutDataGraph(rows) {
     let graphicPizzadata = []
     let jsonData = {}
-    
+    let jsonDataOrdered = {};
+    let jsonDataResumed = {};
+    let sumTotal = 0;
+
     for(let row of rows) {
       
-      if(row["Period Desc."] === "2017"  ) {
+      if(row["Period Desc."] === this.state.yearSelected
+          && row["Trade Flow"] === this.state.tradeFlowSelected  ) {
         if(!jsonData[jsonData["Commodity"]]) {   
           jsonData[row["Commodity"]] = Number(row["Trade Value (US$)"])
         } else {
@@ -80,28 +94,126 @@ class Analysis extends Component {
       }
     }
     
-    for(let key in jsonData) {
-      graphicPizzadata.push([key, jsonData[key]])
+    //ORDER JSON DATA
+    let jsonDataKeysOrdered = Object.keys(jsonData).sort(function(a, b) {
+      return jsonData[a] > jsonData[b] ? -1 : 1
+    })
+    for(let jsonDataKey of jsonDataKeysOrdered) {
+        jsonDataOrdered[jsonDataKey] = jsonData[jsonDataKey];
+        sumTotal += jsonDataOrdered[jsonDataKey];
+    }
+
+    if(sumTotal === 0) {
+      return []
+    }
+
+    //CALCULATE THE OTHERS IN GRAPH
+    jsonDataResumed['Others'] = 0;
+    let sumTotal20PercentValue = sumTotal*0.2;
+    for(let i = jsonDataKeysOrdered.length - 1 ; i >= 0 ; i--) {
+      if(jsonDataResumed['Others'] < sumTotal20PercentValue) {
+        jsonDataResumed['Others'] += jsonDataOrdered[jsonDataKeysOrdered[i]];
+      }else{
+        jsonDataResumed[jsonDataKeysOrdered[i]] = jsonDataOrdered[jsonDataKeysOrdered[i]]
+      }
+    }
+
+    //ORDER JSON DATA RESUMED
+    jsonDataOrdered = {};
+    jsonDataKeysOrdered = Object.keys(jsonDataResumed).sort(function(a, b) {
+      return jsonDataResumed[a] > jsonDataResumed[b] ? -1 : 1
+    })
+    for(let jsonDataKey of jsonDataKeysOrdered) {
+        jsonDataOrdered[jsonDataKey] = jsonDataResumed[jsonDataKey];
+    }
+
+    //PUT OTHERS AS LAST ELEMENT IN JSON
+    let othersValue = jsonDataOrdered['Others'];
+    delete jsonDataOrdered['Others'];
+    jsonDataOrdered['Others'] = othersValue
+
+    //PUT DATA IN GRAPH
+    for(let key in jsonDataOrdered) {
+      graphicPizzadata.push([key, jsonDataOrdered[key]])
     }
     return graphicPizzadata
   }
 
   componentDidMount(){
     this.GetData().then(res => {
-      options.series[0].data = this.PutDataGraph(res);
-      let newOptions = {...options}
-        newOptions.series[0].data = this.PutDataGraph(res);
-        this.setState({graphicOptions: newOptions});
-      })
+      this.state.rows = res;
+      
+    })
+  }
+
+  onSelectYear(ev) {
+    this.setState({...this.state, yearSelected: ev.yearSelected})
+  }
+
+  onSelectTradeFlow(ev) {
+    this.setState({...this.state, tradeFlowSelected: ev.tradeFlowSelected})
+  }
+
+  refreshGraph(graphicOptions) {
+    this.setState({...this.state, showGraph: false, graphicOptions: graphicOptions});
+    setTimeout(() => {
+      this.setState({...this.state, showGraph: true})
+    },0)
+  }
+
+  executeFilter() {
+    let newOptions = {...options}
+    newOptions.series[0].data = this.PutDataGraph(this.state.rows);
+    this.refreshGraph(newOptions)
   }
 
   render(){
+
+    const {graphicOptions, showGraph} = this.state;
         return(
-            
-                <HighchartsReact
-                highcharts={Highcharts}
-                options={this.state.graphicOptions}
-                />
+            <div>
+              <div className="filters-wrapper" >
+
+              <div className="p-grid">
+                  <div className="p-col">
+                    <div>
+                      <Dropdown value={this.state.yearSelected} 
+                        options={yearOptions} 
+                        onChange={(e) => {this.onSelectYear({yearSelected: e.value})}} 
+                        placeholder="Select a Year"/>
+                    </div>
+                  </div>
+
+
+                  <div className="p-col">
+                    <div>
+                      <Dropdown value={this.state.tradeFlowSelected} 
+                        options={tradeFlowOptions} 
+                        onChange={(e) => {this.onSelectTradeFlow({tradeFlowSelected: e.value})}} 
+                        placeholder="Select a trade flow"/>
+                    </div>
+                  </div>
+
+                  <div className="p-col">
+                    <Button onClick={() => { this.executeFilter() }}
+                      label="Filter" className="p-button-raised p-button-rounded"
+                      icon="pi pi-search" iconPos="left" />
+                  </div>
+
+              </div>
+
+              </div>
+  
+              <div className="graph-wrapper" >
+                { showGraph ?  
+                  (<HighchartsReact
+                    highcharts={Highcharts}
+                    options={graphicOptions}
+                    />
+                    ):<></>}
+                  
+              </div>
+            </div>
             
         )
     }
